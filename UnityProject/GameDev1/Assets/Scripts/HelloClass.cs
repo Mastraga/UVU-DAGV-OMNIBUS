7629b89f247b29bb6199e0ddf26bb775c155c6e6
Mason S. <10767492@uvu.edu>
2025-08-27 14:50:53 -0600

    Test script for Unity

---
+++ UnityProject/GameDev1/Assets/Scripts/HelloClass.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelloClass : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello from What if we added fishing?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
